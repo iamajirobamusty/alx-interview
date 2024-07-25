1c10102fe23ba4ff66032c326e979dccacffd504
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-07-25 15:19:29 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,7 +13,9 @@ def def validUTF8(data):
 
   #loop through the data to be validated
   for i in data:
+   #initialize the binary representaion of the data
     check = bin(i).lstrip('0b')
+   #check if the LSF == 00000000
     if check[1:] == "00000000":
       return False
     return True
