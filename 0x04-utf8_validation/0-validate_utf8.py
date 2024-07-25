123d37c50018922ba685044ef61a319597372d56
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-07-25 15:16:24 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,13 +5,14 @@ Method to check if a data utf-8 validated
 """
 
 def def validUTF8(data):
+ """_summary_
+
+    Args:
+            data (list[int]): a list of integers
   """
-  function to receive data as parameter
-  """
+
+  #loop through the data to be validated
   for i in data:
-    """
-    loop through the data to be validated
-    """
     check = bin(i).lstrip('0b')
     if check[1:] == "00000000":
       return False
